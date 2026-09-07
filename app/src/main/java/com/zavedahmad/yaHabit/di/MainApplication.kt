68f01d4a1eefa5c0550c0719cf58306fca5200b2
binbot <binbot@gmail.com>
2026-09-07 15:28:57 -0400

    notif: create habit_reminders channel + boot/alarm receiver stubs

--- app/src/main/java/com/zavedahmad/yaHabit/di/MainApplication.kt
+++ app/src/main/java/com/zavedahmad/yaHabit/di/MainApplication.kt
@@ -2,7 +2,9 @@ package com.zavedahmad.yaHabit.di
 
 import android.app.Application
 
+import com.zavedahmad.yaHabit.notification.NotificationHelper
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.get
 import org.koin.core.context.startKoin
 
 
@@ -14,5 +16,7 @@ class MainApplication : Application(){
             modules(databaseUtilsModule, mainDBModule, habitRepositoryModule)
 
         }
+        // Create notification channel early (minSdk 26, no compat branching needed)
+        get().get<NotificationHelper>().createChannel()
     }
 }
---
+++ app/src/main/java/com/zavedahmad/yaHabit/notification/AlarmReceiver.kt
@@ -0,0 +1,11 @@
+package com.zavedahmad.yaHabit.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Stub for step/notify-channel-perms: actual suppression + notification will be added in scheduler step.
+    }
+}
